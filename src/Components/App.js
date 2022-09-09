7b1aa308f3269a7a9bdc60e00a8eab107df8cfdc
ks1912 <kunalsingh886312@gmail.com>
2022-09-09 17:02:58 +0530

    added id

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div className="container font-monospace">
       <div className="p-5 m-5"></div>
-      <div className="p-md-5 m-md-5"></div>
       <div className="p-md-5 m-md-5">
         <div className="row">
           <span className="text-center display-5 text-white">
